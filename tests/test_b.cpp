2a55431f57f48f7bf639791d0216f735e0cfebc7
David Cattermole <cattermole91@gmail.com>
2020-11-28 22:30:04 +0000

    Change name of function, to bring it in-line with previous change.

--- tests/test_b.cpp
+++ tests/test_b.cpp
@@ -6,7 +6,7 @@ namespace ocg = opencompgraph;
 
 int test_b() {
     auto app_name = std::string("my awesome demo");
-    auto x = ocg::make_demo(app_name);
+    auto x = ocg::make_thingc(app_name);
     auto name = ocg::get_name(*x);
     std::cout << name << std::endl;
     return 0;
