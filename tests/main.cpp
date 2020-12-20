eb783ce7f450e2c24677f6807b17eb71dcdcc0c1
David Cattermole <cattermole91@gmail.com>
2020-12-20 01:33:04 +0000

    Testing executable print-out change.

--- tests/main.cpp
+++ tests/main.cpp
@@ -6,8 +6,7 @@
 #include "test_e.h"
 
 int main() {
-    int x = 2 + 2;
-    std::cout << "2 + 2 = " << x << std::endl;
+    std::cout << "Starting Tests..." << std::endl;
 
     // Run tests.
     test_a();
@@ -16,6 +15,6 @@ int main() {
     test_d();
     test_e();
 
-    std::cout << "Completed Tests" << std::endl;
+    std::cout << "Completed Tests!" << std::endl;
     return 0;
 }
