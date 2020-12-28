c31c8162b63334438faddaa2d1907e91ffce1e4d
David Cattermole <cattermole91@gmail.com>
2020-12-28 09:56:44 +0000

    Change build variables for easier CMake sub-project inclusion.

--- build_windows64.bat
+++ build_windows64.bat
@@ -4,24 +4,24 @@ SETLOCAL
 
 :: The root of this project.
 SET THIS_DIR=%~dp0
-SET ROOT=%THIS_DIR%
-ECHO Package Root: %ROOT%
-CHDIR %ROOT%
+SET OCG_ROOT=%THIS_DIR%
+ECHO OpenCompGraph Root: %OCG_ROOT%
+CHDIR %OCG_ROOT%
 
 :: Install directory
-SET INSTALL_DIR="%ROOT%\install"
+SET INSTALL_DIR="%OCG_ROOT%\install"
 
 :: Where to find the Rust libraries and headers.
-SET RUST_BUILD_DIR="%ROOT%\target\release"
-SET RUST_INCLUDE_DIR="%ROOT%\include"
+SET RUST_BUILD_DIR="%OCG_ROOT%\target\release"
+SET RUST_INCLUDE_DIR="%OCG_ROOT%\include"
 
 :: Build Rust
 ::
 :: Assumes 'cxxbridge-cmd' and 'cbindgen' is installed.
-cxxbridge --header --output "%ROOT%\include\rust\cxx.h"
+cxxbridge --header --output "%OCG_ROOT%\include\rust\cxx.h"
 cbindgen --config cbindgen.toml ^
          --crate opencompgraph ^
-         --output "%ROOT%\include\opencompgraph\_cbindgen.h"
+         --output "%OCG_ROOT%\include\opencompgraph\_cbindgen.h"
 cargo build --release
 
 :: Build C++
@@ -29,7 +29,7 @@ MKDIR build_windows
 CHDIR build_windows
 :: HACK: Create empty file so that CMake can use add_executable, but
 :: the file contents have not yet been written.
-cmake -E touch "%ROOT%\src\_cxxbridge.cpp"
+cmake -E touch "%OCG_ROOT%\src\_cxxbridge.cpp"
 cmake -G "NMake Makefiles" ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DCMAKE_INSTALL_PREFIX=%INSTALL_DIR% ^
@@ -42,4 +42,4 @@ nmake /F Makefile all
 nmake /F Makefile install
 
 :: Return back project root directory.
-CHDIR "%ROOT%"
+CHDIR "%OCG_ROOT%"
