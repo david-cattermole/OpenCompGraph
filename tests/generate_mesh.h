5780dff5849fdc816cf93319db8b326e5fcc015f
David Cattermole <cattermole91@gmail.com>
2021-01-10 02:58:59 +0000

    Add missing import guard to stop duplicate symbol linking errors.

--- tests/generate_mesh.h
+++ tests/generate_mesh.h
@@ -1,4 +1,6 @@
-#include <iostream>
+#ifndef OPENCOMPGRAPH_TEST_GENERATE_MESH_H
+#define OPENCOMPGRAPH_TEST_GENERATE_MESH_H
+
 #include <opencompgraph.h>
 
 namespace ocg = open_comp_graph;
@@ -12,3 +14,5 @@ void generate_mesh(
     size_t &pos_count,
     size_t &uv_count,
     size_t &tri_count);
+
+#endif //OPENCOMPGRAPH_TEST_GENERATE_MESH_H
