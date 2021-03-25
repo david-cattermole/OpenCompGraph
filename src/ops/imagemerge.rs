29670d60e3993724493e58a29f75ad01ab5f3f82
David Cattermole <cattermole91@gmail.com>
2021-03-25 20:31:01 +0000

    Add license header to each file.

--- src/ops/imagemerge.rs
+++ src/ops/imagemerge.rs
@@ -1,3 +1,24 @@
+/*
+ * Copyright (C) 2020, 2021 David Cattermole.
+ *
+ * This file is part of OpenCompGraph.
+ *
+ * OpenCompGraph is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * OpenCompGraph is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with OpenCompGraph.  If not, see <https://www.gnu.org/licenses/>.
+ * ====================================================================
+ *
+ */
+
 use log::{debug, error, info, warn};
 use std::time::{Duration, Instant};
 
