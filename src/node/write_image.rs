8cc02ae0265e242212528d169281fca4696cad8f
David Cattermole <cattermole91@gmail.com>
2021-09-11 19:54:07 +0100

    Add write node with 'execute' attribute to stop node from auto writing.

--- src/node/write_image.rs
+++ src/node/write_image.rs
@@ -62,9 +62,23 @@ pub fn new(id: Identifier) -> NodeImpl {
 #[derive(Debug, Clone, Default)]
 pub struct WriteImageOperation {}
 
+// TODO: Add attributes:
+//
+// - Pre-multiply (?)
+// - Output Channels
+// - Output colour space.
+// - Compression...
+//   - JPEG Compression Level
+//   - JPEG Sub-sample
+//   - PNG Compression level
+//   - EXR Compression method
+//   - EXR Compression DWA Level
+// -
+//
 #[derive(Debug, Clone, Default, hash::Hash)]
 pub struct WriteImageAttrs {
     pub enable: i32,
+    pub execute: i32,
     pub file_path: String,
     // Crop the input image the display window before writing?
     //
@@ -75,14 +89,8 @@ pub struct WriteImageAttrs {
     // When 'auto' is enabled, if the image format to be writen does
     // not support a data windows, the image is cropped, otherwise it
     // is not.
-    pub crop_on_write: i32,
+    pub crop_on_write: i32, // TODO: Create a enum.
 
-    // TODO: Add attributes:
-    //
-    // - Pre-multiply (?)
-    // - Output Channels
-    // - Output colour space.
-    //
     // The pixel data type for pixels written out.
     pub pixel_data_type: i32, // index for PixelDataType.
 }
@@ -97,11 +105,95 @@ impl WriteImageAttrs {
     pub fn new() -> WriteImageAttrs {
         WriteImageAttrs {
             enable: 1,
+            execute: 1,
             file_path: "".to_string(),
             crop_on_write: -1,
             pixel_data_type: 255, // 255 = PixelDataType::Unknown
         }
     }
+}
+
+fn do_image_process(
+    input: &Rc<StreamDataImpl>,
+    file_path: &str,
+    frame: FrameValue,
+    bake_pixel_data_type: PixelDataType,
+    crop_on_write: i32,
+) -> bool {
+    let frame_num = frame.round().trunc() as i32;
+    let path_expanded = pathutils::expand_string(file_path.to_string(), frame_num);
+
+    // Copy input data
+    let mut copy = &mut (**input).clone();
+    let display_window = copy.display_window();
+    let mut data_window = copy.data_window();
+    let mut pixel_block = copy.clone_pixel_block();
+    let mut image_spec = copy.clone_image_spec();
+    let from_color_space = &image_spec.color_space();
+
+    // The Pixels will be baked into this data type before
+    // writing out.
+    let out_pixel_data_type = match bake_pixel_data_type {
+        PixelDataType::Unknown => copy.pixel_data_type(),
+        _ => bake_pixel_data_type,
+    };
+
+    // The color space to write to.
+    let to_color_space = if path_expanded.ends_with(".jpg")
+        || path_expanded.ends_with(".png")
+        || path_expanded.ends_with(".jpeg")
+        || path_expanded.ends_with(".jpe")
+        || path_expanded.ends_with(".jif")
+        || path_expanded.ends_with(".jfif")
+        || path_expanded.ends_with(".jfi")
+    {
+        COLOR_SPACE_NAME_SRGB.to_string()
+    } else {
+        COLOR_SPACE_NAME_LINEAR.to_string()
+    };
+
+    let bake_option = BakeOption::All;
+    ops::bake::do_process(
+        bake_option,
+        &mut pixel_block,
+        display_window,
+        &mut data_window,
+        &mut image_spec,
+        &mut copy,
+        &from_color_space,
+        &to_color_space,
+        out_pixel_data_type,
+    );
+
+    // Write pixels
+    let pixel_block_box = Box::new(pixel_block);
+    let image = ImageShared {
+        pixel_block: pixel_block_box,
+        display_window,
+        data_window,
+        spec: image_spec,
+    };
+    let num_threads = 0;
+
+    // Should the image be cropped to the display window
+    // before writing?
+    let do_crop = match crop_on_write {
+        0 => false, // Disabled
+        1 => true,  // Enabled
+        -1 => {
+            // Auto.
+            //
+            // When If the image format does not support data
+            // windows, then crop off the extra information outside
+            // the display window.
+            let format_supports_data_window = path_expanded.ends_with(".exr");
+            !format_supports_data_window
+        }
+        _ => panic!("Invalid crop_on_write value: {}", crop_on_write),
+    };
+
+    let ok = imageio::write_image(&image, &path_expanded, num_threads, do_crop);
+    ok
 }
 
 impl Operation for WriteImageOperation {
@@ -140,91 +232,31 @@ impl Operation for WriteImageOperation {
                 NodeStatus::Warning
             }
             _ => {
-                let input = &inputs[0].clone();
-
-                let file_path = attr_block.get_attr_str("file_path");
-                let frame_num = frame.round().trunc() as i32;
-                let path_expanded = pathutils::expand_string(file_path.to_string(), frame_num);
-
-                // Copy input data
-                let mut copy = &mut (**input).clone();
-                let display_window = copy.display_window();
-                let mut data_window = copy.data_window();
-                let mut pixel_block = copy.clone_pixel_block();
-                let mut image_spec = copy.clone_image_spec();
-                let from_color_space = &image_spec.color_space();
-
-                // The Pixels will be baked into this data type before
-                // writing out.
-                let bake_pixel_data_type =
-                    PixelDataType::from(attr_block.get_attr_i32("pixel_data_type"));
-                let out_pixel_data_type = match bake_pixel_data_type {
-                    PixelDataType::Unknown => copy.pixel_data_type(),
-                    _ => bake_pixel_data_type,
-                };
-
-                // The color space to write to.
-                let to_color_space = if path_expanded.ends_with(".jpg")
-                    || path_expanded.ends_with(".png")
-                    || path_expanded.ends_with(".jpeg")
-                    || path_expanded.ends_with(".jpe")
-                    || path_expanded.ends_with(".jif")
-                    || path_expanded.ends_with(".jfif")
-                    || path_expanded.ends_with(".jfi")
-                {
-                    COLOR_SPACE_NAME_SRGB.to_string()
+                let execute = attr_block.get_attr_i32("execute") != 0;
+                if execute == false {
+                    *output = inputs[0].clone();
+                    NodeStatus::Valid
                 } else {
-                    COLOR_SPACE_NAME_LINEAR.to_string()
-                };
-
-                let bake_option = BakeOption::All;
-                ops::bake::do_process(
-                    bake_option,
-                    &mut pixel_block,
-                    display_window,
-                    &mut data_window,
-                    &mut image_spec,
-                    &mut copy,
-                    &from_color_space,
-                    &to_color_space,
-                    out_pixel_data_type,
-                );
-
-                // Write pixels
-                *output = inputs[0].clone();
-                let pixel_block_box = Box::new(pixel_block);
-                let image = ImageShared {
-                    pixel_block: pixel_block_box,
-                    display_window,
-                    data_window,
-                    spec: image_spec,
-                };
-                let num_threads = 0;
-                let crop_on_write = attr_block.get_attr_i32("crop_on_write");
-
-                // Should the image be cropped to the display window
-                // before writing?
-                let do_crop = match crop_on_write {
-                    0 => false, // Disabled
-                    1 => true,  // Enabled
-                    -1 => {
-                        // Auto.
-                        //
-                        // When If the image format does not support data
-                        // windows, then crop off the extra information outside
-                        // the display window.
-                        let format_supports_data_window = path_expanded.ends_with(".exr");
-                        !format_supports_data_window
+                    let input = &inputs[0].clone();
+                    let file_path = attr_block.get_attr_str("file_path");
+                    let bake_pixel_data_type =
+                        PixelDataType::from(attr_block.get_attr_i32("pixel_data_type"));
+                    let crop_on_write = attr_block.get_attr_i32("crop_on_write");
+                    let ok = do_image_process(
+                        input,
+                        file_path,
+                        frame,
+                        bake_pixel_data_type,
+                        crop_on_write,
+                    );
+                    if ok == false {
+                        warn!("Failed to write image: status={}", ok);
                     }
-                    _ => panic!("Invalid crop_on_write value: {}", crop_on_write),
-                };
+                    debug!("Success: {}", ok);
 
-                let ok = imageio::write_image(&image, &path_expanded, num_threads, do_crop);
-                if ok == false {
-                    warn!("Failed to write image: status={}", ok);
+                    *output = inputs[0].clone();
+                    NodeStatus::Valid
                 }
-                debug!("Success: {}", ok);
-                NodeStatus::Valid
             }
         }
     }
@@ -243,6 +275,7 @@ impl AttrBlock for WriteImageAttrs {
     fn attr_exists(&self, name: &str) -> AttrState {
         match name {
             "enable" => AttrState::Exists,
+            "execute" => AttrState::Exists,
             "file_path" => AttrState::Exists,
             "crop_on_write" => AttrState::Exists,
             "pixel_data_type" => AttrState::Exists,
@@ -267,6 +300,7 @@ impl AttrBlock for WriteImageAttrs {
     fn get_attr_i32(&self, name: &str) -> i32 {
         match name {
             "enable" => self.enable,
+            "execute" => self.execute,
             "crop_on_write" => self.crop_on_write,
             "pixel_data_type" => self.pixel_data_type,
             _ => 0,
@@ -276,6 +310,7 @@ impl AttrBlock for WriteImageAttrs {
     fn set_attr_i32(&mut self, name: &str, value: i32) {
         match name {
             "enable" => self.enable = value,
+            "execute" => self.execute = value,
             "crop_on_write" => self.crop_on_write = value,
             "pixel_data_type" => self.pixel_data_type = value,
             _ => (),
